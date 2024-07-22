bac064ed659479d30aaffc2a2ddf153cc1f0cfaa
unknown <russell.chen@speciband.com>
2024-07-22 19:49:16 +0800

    修改Albumentations库的数据增强概率

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -30,12 +30,12 @@ class Albumentations:
 
             T = [
                 A.RandomResizedCrop(height=size, width=size, scale=(0.8, 1.0), ratio=(0.9, 1.11), p=0.0),
-                A.Blur(p=0.01),
-                A.MedianBlur(p=0.01),
-                A.ToGray(p=0.01),
-                A.CLAHE(p=0.01),
-                A.RandomBrightnessContrast(p=0.0),
-                A.RandomGamma(p=0.0),
+                A.Blur(p=0.1),
+                A.MedianBlur(p=0.1),
+                A.ToGray(p=0.1),
+                A.CLAHE(p=0.1),
+                A.RandomBrightnessContrast(p=0.5),
+                A.RandomGamma(p=0.3),
                 A.ImageCompression(quality_lower=75, p=0.0)]  # transforms
             self.transform = A.Compose(T, bbox_params=A.BboxParams(format='yolo', label_fields=['class_labels']))
 
